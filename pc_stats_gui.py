82afd7860459ad3bfe28711b7d483aa609f6141e
enl9076 <enl9076@gmail.com>
2024-07-08 07:16:35 -0400

    fixed the update function

--- pc_stats_gui.py
+++ pc_stats_gui.py
@@ -27,39 +27,51 @@ def get_storage_info():
 class Dashboard(ttb.Frame):
     def __init__(self, main):
         super().__init__(main, padding=(20,20))
-        self.pack(fill=BOTH, expand=YES)
+        self.place(relheight=1, relwidth=1)
+        self.columnconfigure((0,1,2,3,4), weight=1)
+        self.rowconfigure((0,1,2), weight=2)
+        #self.pack(fill=BOTH, expand=YES)
         self.board = platform.node()
         self.cpu = cpuinfo.get_cpu_info()['brand_raw']
         self.num_cores = cpuinfo.get_cpu_info()['count']/2
         self.num_threads = cpuinfo.get_cpu_info()['count']
         self.proc_speed = cpuinfo.get_cpu_info()['hz_actual_friendly']
-        self.total_ram = psutil.virtual_memory().total/1024**3
+        self.total_ram = int(psutil.virtual_memory().total/1024**3)
         self.available_disks = [d[0] for d in psutil.disk_partitions()]
         self.cpu_usage = psutil.cpu_percent()
         self.ram_usage = psutil.virtual_memory().percent
 
-        self.create_label(self.board)
-        self.create_label(self.cpu)
-        self.create_label(self.total_ram)
-        self.create_label(f'{len(self.available_disks)} available disks')
-        #self.create_label(self.proc_speed)
+        self.create_label(self.board, col_num=1, row_num=0)
+        self.create_label(self.cpu, col_num=1, row_num=0)
+        self.create_label(self.total_ram, col_num=2, row_num=0)
+        self.create_label(f'{len(self.available_disks)} available disks', col_num=3, row_num=0)
+        self.create_label(self.proc_speed, col_num=0, row_num=0)
 
-        self.create_meter("CPU Usage", self.cpu_usage)
-        self.create_meter("RAM Usage", self.ram_usage)
+        self.cpu_meter = self.create_meter("CPU Usage", self.cpu_usage, col_num=1, row_num=1)
+        self.ram_meter = self.create_meter("RAM Usage", self.ram_usage, col_num=2, row_num=1)
 
-    def create_label(self, label):
-        lab = ttb.Label(self, text=label)
-        lab.pack()
+        self.update()
 
-    def create_meter(self, label, variable):
+    def create_label(self, label, col_num, row_num):
+        lab = ttb.Label(self, text=label, font=('Helvetica', 12))
+        lab.grid(column=col_num, row=row_num)
+
+    def create_meter(self, label, variable, col_num, row_num):
         meter = ttb.Meter(self, metersize=150, padding = 10, amounttotal=100, metertype="semi", interactive=False, subtext=label, textright="%")
-        meter.pack()
+        meter.grid(column=col_num,row=row_num)
         meter.configure(amountused=variable)
+        return meter
 
     def update(self):
-        return
+        self.cpu_usage = psutil.cpu_percent()
+        self.ram_usage = psutil.virtual_memory().percent
+        self.cpu_meter.configure(amountused=self.cpu_usage)
+        self.ram_meter.configure(amountused=self.ram_usage)
+        self.after(2000, self.update)
+
     
 if __name__ == "__main__":
     app = ttb.Window("Dashboard", "darkly")
     Dashboard(app)
+    app.geometry('800x800')
     app.mainloop()
